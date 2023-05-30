92ddc661c304c73b7ea4432ec52f60c249b21420
Le Duc <102973625+HieuLeDuc@users.noreply.github.com>
2023-05-30 13:52:08 +0200

    revert to small scale testing

--- main/src/main/java/com/geocontentanalyser/App.java
+++ main/src/main/java/com/geocontentanalyser/App.java
@@ -16,6 +16,7 @@ public class App {
         //List<String> wikiURLlList = WikiScrapperMain.crawler();
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
         
+        /* 
         //read each line from t.log and save to a list
         List<String> wikiURLlList = new ArrayList<String>();
         BufferedReader reader = new BufferedReader(new FileReader("output/2023-05-24T11-57.log"));
@@ -30,14 +31,10 @@ public class App {
             SiteURLExtractor siteURLExtractor = new SiteURLExtractor(URL);
             executor.execute(siteURLExtractor);
         }
-
-        /* 
+        */  
+        
         SiteURLExtractor siteURLExtractor = new SiteURLExtractor("https://www.kreis-mettmann.de/");
-        for (String URL : wikiURLlList) {
-            SiteURLExtractor siteURLExtractor = new SiteURLExtractor(URL);
-            Thread thread = new Thread(siteURLExtractor);
-            thread.start();
-        }
-        */      
+        Thread thread = new Thread(siteURLExtractor);
+        thread.start();            
     }
 }
